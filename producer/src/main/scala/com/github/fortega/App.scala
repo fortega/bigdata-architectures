dc1857941e0662bc4074c116a6edcb501f4fb144
Felipe Ortega <felipeortegabustamante@gmail.com>
2022-11-23 18:27:18 -0300

    feat(producer): change to interval logic

--- producer/src/main/scala/com/github/fortega/App.scala
+++ producer/src/main/scala/com/github/fortega/App.scala
@@ -9,27 +9,38 @@ import scala.util.Success
 
 object App {
   def main(cmdArgs: Array[String]): Unit = cmdArgs match {
-    case Array(server, queue, msgs) if Try(msgs.toInt).isSuccess =>
+    case Array(server, queue, period) if Try(period.toInt).isSuccess =>
       usingChannel(server) { channel =>
-        channel.queueDeclare(queue, false, false, false, null)
+        channel.queueDeclare(queue, true, false, false, null)
 
-        Stream
-          .range(0, msgs.toInt)
-          .map(createEvent)
-          .map(_.toByteArray)
-          .foreach(channel.basicPublish("", queue, null, _))
+        createTimer(period.toLong) { () =>
+          val event = randomEvent
+          channel.basicPublish("", queue, null, event.toByteArray)
+        }
+
+        loop
       }
     case _ => sys.error("invalid arguments")
   }
 
-  private def createEvent(i: Int): Event = Event(
-    deviceId = i % 10,
-    time = i * 10L,
-    longitude = (i % 360) - 180,
-    latitude = (i % 180) - 90,
-    altitude = (i % 500).toDouble,
-    velocity = (i % 200),
-    angle = (i % 360)
+  private def loop = while (true) { Thread.sleep(Long.MaxValue) }
+
+  private def createTimer(period: Long)(f: () => Unit) = {
+    val task = new java.util.TimerTask() {
+      override def run = f()
+    }
+    val timer = new java.util.Timer(true)
+    timer.scheduleAtFixedRate(task, 0, period)
+  }
+
+  private def randomEvent = Event(
+    deviceId = (100 * math.random).toInt,
+    time = System.currentTimeMillis,
+    longitude = (360 * math.random) - 180,
+    latitude = (180 * math.random) - 90,
+    altitude = (100 * math.random),
+    velocity = (200 * math.random).toInt,
+    angle = (360 * math.random).toInt
   )
 
   private def usingChannel[A](
