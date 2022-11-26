c8f6cc91a6252edf6022e8f655603d0d03f34c53
Felipe Ortega <felipeortegabustamante@gmail.com>
2022-11-26 17:29:18 -0300

    feat: lambda to amqp protocol to see ack locks

--- build.sbt
+++ build.sbt
@@ -7,44 +7,46 @@ ThisBuild / scalaVersion := "2.12.15"
 // Cats
 scalacOptions += "-Ypartial-unification"
 
+// Dependencies
+val amqpClient = "com.rabbitmq" % "amqp-client" % "5.16.0"
+val sparkSql = "org.apache.spark" %% "spark-sql" % "3.3.1"
+val cats = "org.typelevel" %% "cats-core" % "2.9.0"
+val slf4j = "org.slf4j" % "slf4j-simple" % "2.0.5"
+val scalaTest = "org.scalatest" %% "scalatest" % "3.2.14"
+
+// Projects
 val root = (project in file("."))
   .settings(
     name := "bigdata-architectures-protobuf"
   )
 
-// Projects
 val core = (project in file("core"))
   .dependsOn(root)
   .settings(
     name := "bigdata-architectures-core",
-    libraryDependencies ++= Seq(
-      "org.typelevel" %% "cats-core" % "2.9.0",
-      "org.scalatest" %% "scalatest" % "3.2.14" % Test
-    )
+    libraryDependencies ++= Seq(cats, slf4j),
+    libraryDependencies += scalaTest % Test
   )
 
 val batch = (project in file("batch"))
   .dependsOn(core)
   .settings(
     name := "bigdata-architectures-batch",
-    libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.3.1"
+    libraryDependencies += sparkSql
   )
+
 val producer = (project in file("producer"))
   .dependsOn(core)
   .settings(
     name := "gps-events-producer",
-    libraryDependencies += "com.rabbitmq" % "amqp-client" % "5.16.0"
+    libraryDependencies += amqpClient
   )
 
-val flinkVersion = "1.16.0"
 val lambda = (project in file("lambda"))
   .dependsOn(core)
   .settings(
     name := "bigdata-architectures-lambda",
-    libraryDependencies ++= Seq(
-      "org.apache.flink" %% "flink-streaming-scala" % flinkVersion,
-      "org.apache.flink" % "flink-clients" % flinkVersion
-    )
+    libraryDependencies += amqpClient
   )
 
 // ScalaPB
--- lambda/src/main/scala/com/github/fortega/App.scala
+++ lambda/src/main/scala/com/github/fortega/App.scala
@@ -1,38 +1,124 @@
 package com.github.fortega
 
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
 import scala.util.{Try, Failure, Success}
-import com.github.fortega.model.gps.Event
+import com.github.fortega.model.gps.{Event, ValidatedEvent}
 import com.github.fortega.types.InvalidReasonInstances._
 import com.github.fortega.types.InvalidReasonSyntax._
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator
-import org.apache.flink.streaming.api.functions.sink.PrintSink
-import org.apache.flink.streaming.api.functions.sink.SinkFunction
+import com.rabbitmq.client.{
+  ConnectionFactory,
+  Channel,
+  Consumer,
+  ShutdownSignalException,
+  Envelope
+}
+import com.rabbitmq.client.AMQP.BasicProperties
+import org.slf4j.LoggerFactory
 
 object App {
-  def main(cmdArgs: Array[String]): Unit = getEnv.map { env =>
-    val events = env.fromSequence(1, 100).map(i => fakeEvent(i))
+  private lazy val log = LoggerFactory.getLogger(this.getClass)
+  private lazy val exchange = ""
+  def main(cmdArgs: Array[String]): Unit = cmdArgs match {
+    case Array(host, inputQueue, deadLetterQueue, validQueue, invalidQueue) =>
+      usingChannel(host) { channel =>
+        log.info("declaring queues")
+        Seq(deadLetterQueue, validQueue, invalidQueue).foreach { queue =>
+          channel.queueDeclare(queue, true, false, false, null)
+          log.info(s"$queue declared")
+        }
 
-    events
-      .map { _.validated.toString }
-      .print()
-    env.execute
-  } match {
-    case Failure(error) => sys.error(s"error: ${error.getMessage()}")
-    case Success(value) => println("success")
+        log.info("starting consumer")
+        channel.basicConsume(
+          inputQueue,
+          consumer(channel, deadLetterQueue) { case (properties, body) =>
+            val event = Event.parseFrom(body)
+            val (output: String, newBody: Array[Byte]) =
+              event.invalidReason match {
+                case None => (validQueue, body)
+                case Some(invalidReason) =>
+                  (
+                    invalidQueue,
+                    ValidatedEvent(event, Some(invalidReason)).toByteArray
+                  )
+              }
+            channel.basicPublish(exchange, output, properties, newBody)
+          }
+        )
+
+        log.info("infinite loop")
+        loop
+      }
+
+    case _ => sys.error("Invalid arguments")
   }
 
-  private def fakeEvent(i: Long) = Event(
-    deviceId = (i % 10).toInt,
-    time = i * 10,
-    longitude = i.toDouble,
-    latitude = i.toDouble,
-    altitude = i.toDouble,
-    velocity = i.toInt,
-    angle = (i % 360).toInt
-  )
+  private def consumer(
+      channel: Channel,
+      deadLetterQueue: String
+  )(
+      f: (BasicProperties, Array[Byte]) => Unit
+  ): Consumer = new Consumer() {
+    private lazy val log = LoggerFactory.getLogger(this.getClass)
 
-  private def getEnv: Try[StreamExecutionEnvironment] = Try {
-    StreamExecutionEnvironment.getExecutionEnvironment
+    override def handleConsumeOk(consumerTag: String): Unit = {
+      log.info(s"starting in $consumerTag")
+    }
+
+    override def handleCancelOk(consumerTag: String): Unit = handleCancel(
+      consumerTag
+    )
+
+    override def handleCancel(consumerTag: String): Unit = {
+      log.info(s"cancel $consumerTag. exiting")
+      sys.exit(0)
+    }
+
+    override def handleShutdownSignal(
+        consumerTag: String,
+        sig: ShutdownSignalException
+    ): Unit = {
+      println(s"shutdown signal($consumerTag): ${sig.getMessage}")
+      sig.printStackTrace
+      sys.exit(1)
+    }
+
+    override def handleRecoverOk(consumerTag: String): Unit = {
+      log.info(s"recovery($consumerTag)")
+    }
+
+    override def handleDelivery(
+        consumerTag: String,
+        envelope: Envelope,
+        properties: BasicProperties,
+        body: Array[Byte]
+    ): Unit = {
+      val tag = envelope.getDeliveryTag
+      Try(f(properties, body)) match {
+        case Failure(error) =>
+          log.error(s"${error.getClass.getName} ${error.getMessage}", error)
+          channel.basicPublish(exchange, deadLetterQueue, properties, body)
+          log.debug(s"dead letter: $tag")
+        case Success(_) =>
+          log.debug(s"success: $tag")
+      }
+      channel.basicAck(tag, false)
+      log.debug(s"ack $tag")
+    }
+
+  }
+
+  private def loop = while (true) Thread.sleep(Long.MaxValue)
+
+  private def usingChannel[A](
+      host: String
+  )(
+      f: Channel => A
+  ): Try[A] = Try {
+    val factory = new ConnectionFactory
+    factory.setHost(host)
+    val connection = factory.newConnection
+    val result = f(connection.createChannel)
+    connection.close()
+
+    result
   }
 }
